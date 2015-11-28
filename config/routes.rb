0ac07a5142032780df5631dfa2426382447e1fbd
Leandro Baltazar <leandrobaltazar@hotmail.de>
2015-11-28 13:03:18 +0100

    add links CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :links
   resources :posts, only: [:index, :show]
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127111108) do
+ActiveRecord::Schema.define(version: 20151128114112) do
+
+  create_table "links", force: :cascade do |t|
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",      null: false
