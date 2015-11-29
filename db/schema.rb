ef85a8981c4dce0e3451a70de2876c984faf52be
Cedrik <dudekcedrik@gmail.com>
2015-11-29 10:54:54 +0100

    documents finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151128140956) do
-
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
